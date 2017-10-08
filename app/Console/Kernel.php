a4c13d6d3a90e3e0d3550e87c5ca4ba6bad100b5
Nivesh Saharan <niveshsaharan@outlook.com>
2017-10-08 19:16:15 +0530

    Dynamic Node url

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('iota:payments:check')->everyFiveMinutes();
+        $schedule->command('iota:payments:check')->everyFifteenMinutes();
     }
 
     /**
