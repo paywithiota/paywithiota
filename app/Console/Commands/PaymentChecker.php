8246f87e506de406bfe29a366bf8155c4a90bb3b
Nivesh Saharan <niveshsaharan@outlook.com>
2017-10-27 18:19:09 +0530

    Update scheduled payment check for metadata

--- app/Console/Commands/PaymentChecker.php
+++ app/Console/Commands/PaymentChecker.php
@@ -57,53 +57,75 @@ class PaymentChecker extends Command
 
         $payments = $payments->get();
 
-        foreach ($payments as $payment) {
+        $totalUnverifiedPayments = count($payments);
+        $totalChecked = 0;
+        $totalVerifiedPayments = 0;
 
-            $metadata = $payment->metadata;
+        $this->info("Total unverified payments: " . $totalUnverifiedPayments);
 
-            // Check if address exists
-            if ($payment->address && (isset($metadata['transaction']['hash']) || $checkAll == 1)) {
+        if ($totalUnverifiedPayments) {
 
-                // Get balance for address
-                $iotaBalance = (new Iota())->getBalanceByAddress($payment->address->address, 'I');
+            $bar = $this->output->createProgressBar($totalUnverifiedPayments);
 
+            foreach ($payments as $payment) {
 
-                // If balance is greater or equals to what it was supposed to
-                if ($iotaBalance >= $payment->price_iota) {
+                $metadata = $payment->metadata;
 
-                    // Update paymetn status as done
-                    $payment->update(['status' => 1]);
+                // Check if address exists
+                if ($payment->address && (isset($metadata['transaction']['hash']) || $checkAll == 1)) {
 
-                    // If IPN URL exists, call it
-                    if ($payment->ipn) {
-                        try{
-                            (new Iota())->call([
-                                'URL'    => $payment->ipn,
-                                'METHOD' => 'POST',
-                                'DATA'   => [
-                                    'address'         => $payment->address->address,
-                                    'address_balance' => $iotaBalance,
-                                    'payment_id'      => base64_encode($payment->id),
-                                    'invoice_id'      => $payment->invoice_id,
-                                    'status'          => $payment->status,
-                                    'price_usd'       => $payment->price_usd,
-                                    'price_iota'      => $payment->price_iota,
-                                    'ipn'             => $payment->ipn,
-                                    'ipn_verify_code' => $payment->ipn_verify_code,
-                                    'custom'          => $payment->metadata,
-                                    'created_at'      => $payment->created_at,
-                                    'updated_at'      => $payment->updated_at
-                                ]
-                            ]);
-                        }catch (\Exception $e){
+                    // Get balance for address
+                    $iotaBalance = (new Iota())->getBalanceByAddress($payment->address->address, 'I');
 
+                    // If balance is greater or equals to what it was supposed to
+                    if ($iotaBalance >= $payment->price_iota) {
+
+                        // Update paymetn status as done
+                        $payment->update(['status' => 1]);
+
+                        // If IPN URL exists, call it
+                        if ($payment->ipn) {
+                            try{
+                                (new Iota())->call([
+                                    'URL'    => $payment->ipn,
+                                    'METHOD' => 'POST',
+                                    'DATA'   => [
+                                        'address'         => $payment->address->address,
+                                        'address_balance' => $iotaBalance,
+                                        'payment_id'      => base64_encode($payment->id),
+                                        'invoice_id'      => $payment->invoice_id,
+                                        'status'          => $payment->status,
+                                        'price_usd'       => $payment->price_usd,
+                                        'price_iota'      => $payment->price_iota,
+                                        'ipn'             => $payment->ipn,
+                                        'ipn_verify_code' => $payment->ipn_verify_code,
+                                        'custom'          => $payment->metadata,
+                                        'created_at'      => $payment->created_at,
+                                        'updated_at'      => $payment->updated_at
+                                    ]
+                                ]);
+                            }catch (\Exception $e){
+
+                            }
                         }
+
+                        // [Event]
+                        event(new PaymentCompleted($payment, []));
+
+                        $totalVerifiedPayments++;
                     }
 
-                    // [Event]
-                    event(new PaymentCompleted($payment, []));
+                    $totalChecked++;
                 }
+
+                $bar->advance();
             }
+
+            $bar->finish();
         }
+
+        $this->info("Checked for verification: " . $totalChecked);
+        $this->info("Verified now: " . $totalVerifiedPayments);
+        die;
     }
 }
